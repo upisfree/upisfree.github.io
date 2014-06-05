1d7f08aa9a6ca20370c1ddac4991046161b9cb43
upisfree <upisfree@outlook.com>
2014-06-05 12:47:08 +0400

    Add more mouse events in symbol-paint

--- symbol-paint/js/main.js
+++ symbol-paint/js/main.js
@@ -12,11 +12,57 @@ function appendSymbol(x, y)
 
 $(function()
 {
+  var left,
+      right;
+
+  // Блокируем контекстное меню
+  $('body').on('contextmenu', function(event)
+  {
+    event.preventDefault();
+  });
+
+  // Изменяем флаг, когда клавиша нажата
+  $(document).mousedown(function(event)
+  {
+    switch (event.which)
+    {
+      case 1:
+        left = true;
+        break;
+      case 3:
+        right = true;
+        break;
+    }
+  });
+
+  // Изменяем флаг, когда клавиша не нажата
+  $(document).mouseup(function(event)
+  {
+    switch (event.which)
+    {
+      case 1:
+        left = false;
+        break;
+      case 3:
+        right = false;
+        break;
+    }
+  });
+
+  // Ну, тут мышку типа двигаем
   $('body').mousemove(function(event)
   {
-    appendSymbol(event.pageX - 8, event.pageY - 20);
-  }).click(function()
+    if (left)
+      appendSymbol(event.pageX - 8, event.pageY - 20);
+
+    $('.symbol').mouseenter(function()
+    {
+      if (right)
+        $(this).remove();
+    });
+  }).click(function(event)
   {
-    $('.symbol').remove();
-  });;
+    if (event.which == 2)
+      $('.symbol').remove();
+  });
 });
