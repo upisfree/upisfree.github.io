54523bf780111596750d2a6f443efc66154964a7
upisfree <upisfree@outlook.com>
2015-09-25 03:32:56 +0600

    Logo's colors

--- site/js/main.js
+++ site/js/main.js
@@ -30,7 +30,21 @@ function resize()
 
 function rgbToHex(r, g, b)
 {
-  return '0x' + r.toString(16) + g.toString(16) + b.toString(16);
+  return '#' + r.toString(16) + g.toString(16) + b.toString(16);
+};
+
+// https://github.com/liabru/matter-js/blob/master/src/core/Common.js#L118
+function shadeColor(color, percent)
+{   
+  // http://stackoverflow.com/questions/5560248/programmatically-lighten-or-darken-a-hex-color
+  var colorInteger = parseInt(color.slice(1),16), 
+      amount = Math.round(2.55 * percent), 
+      R = (colorInteger >> 16) + amount, 
+      B = (colorInteger >> 8 & 0x00FF) + amount, 
+      G = (colorInteger & 0x0000FF) + amount;
+  return '#' + (0x1000000 + (R < 255 ? R < 1 ? 0 : R :255) * 0x10000 
+          + (B < 255 ? B < 1 ? 0 : B : 255) * 0x100 
+          + (G < 255 ? G < 1 ? 0 : G : 255)).toString(16).slice(1);
 };
 
 function noiseOn()
@@ -184,3 +198,18 @@ function showNextVideo()
 
 // start
 loadPlayerAPI();
+
+// logo's things
+var logo = document.getElementById('logo');
+
+logo.onclick = function()
+{
+  showNextVideo();
+};
+
+logo.onmouseover = function()
+{
+  var color = shadeColor(rgbToHex(random(0, 255), random(0, 255), random(0, 255)), -20);
+
+  logo.style.color = logo.style.borderColor = color;
+};
