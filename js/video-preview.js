b203f549db9975fd4873490862921396a8fc4368
Senya Pugach <upisfree@outlook.com>
2026-01-17 15:05:53 +0300

    remove youtube preview, add links to youtube & vk video

--- js/video-preview.js
+++ js/video-preview.js
@@ -1,18 +1,3 @@
-/**
- * @author upisfree / https://upisfr.ee/
- */
-
-const YOUTUBE_BUTTON_SVG = '<svg height="100%" version="1.1" viewBox="0 0 68 48" width="100%"><path class="ytp-large-play-button-bg" d="M 45,24 27,14 27,34" fill="#fff"></path></svg>';
-
-function htmlToElement(html) {
-  html = html.trim();
-
-  const template = document.createElement('template');
-  template.innerHTML = html;
-
-  return template.content.firstChild;
-}
-
 class VideoPreview extends HTMLElement {
   constructor() {
     super();
@@ -29,72 +14,14 @@ class VideoPreview extends HTMLElement {
     if (this.getAttribute('importance')) {
       this.importance = this.getAttribute('importance');
     }
-
-    if (window.onYouTubeIframeAPIReady === undefined) {
-      window.VideoPreviewList = [];
-
-      window.onYouTubeIframeAPIReady = function() {
-        window.VideoPreviewList.forEach(el => el.onIframeAPIReadyLoaded());
-      }
-    }
-
-    window.VideoPreviewList.push(this);
-
-    if (this.getAttribute('youtube')) {
-      this.divId = `youtube-${ this.getAttribute('youtube') }`;
-    } else {
-      this.divId = `youtube-${ window.VideoPreviewList.length }`;
-    }
   }
 
   connectedCallback() {
     this.render();
   }
 
-  onIframeAPIReadyLoaded() {
-    if (!this.getAttribute('youtube')) {
-      return;
-    }
-
-    this.preview.classList.add('loaded');
-    this.playButton.classList.add('loaded');
-  }
-
   onPreviewVideoFirstFrameLoaded() {
     this.previewVideo.classList.add('loaded');
-  }
-
-  onPreviewClick() {
-    if (!this.getAttribute('youtube') || !this.preview.classList.contains('loaded')) {
-      return;
-    }
-
-    this.root.classList.add('clicked');
-    this.preview.classList.add('hidden');
-
-    this.player = new YT.Player(this.divId, {
-      width: this.getAttribute('width'),
-      height: this.getAttribute('height'),
-      videoId: this.getAttribute('youtube'),
-      playerVars: {
-        'autoplay': 1,
-        'color': 'white',
-        'controls': 0,
-        'disablekb': 1,
-        'iv_load_policy': 3,
-        'modestbranding': 1,
-        'playsinline': 1,
-        'rel': 0
-      },
-      events: {
-        onReady: () => {
-          this.player.playVideo();
-        }
-      }
-    });
-
-    // hiding "YouTube video player" title on hover and right after click
-    this.player.getIframe().removeAttribute('title');
   }
 
   render() {
@@ -105,20 +32,12 @@ class VideoPreview extends HTMLElement {
     this.root = document.createElement('div');
 
     this.root.appendChild(this.getStyleTag());
-    this.root.className = this.divId + ' root';
+    this.root.className = 'root';
     this.appendChild(this.root);
-
-    const iframeDiv = document.createElement('div');
-    iframeDiv.id = this.divId;
-    this.root.appendChild(iframeDiv);
 
     this.preview = document.createElement('div');
     this.preview.className = 'preview';
-    this.preview.addEventListener('pointerup', this.onPreviewClick.bind(this));
     this.root.appendChild(this.preview);
-
-    this.playButton = htmlToElement(YOUTUBE_BUTTON_SVG);
-    this.preview.appendChild(this.playButton);
 
     this.previewVideo = document.createElement('video');
     this.previewVideo.autoplay = true;
@@ -143,18 +62,18 @@ class VideoPreview extends HTMLElement {
     const tag = document.createElement('style');
 
     tag.innerHTML = `
-.${ this.divId }.root {
+.root {
   position: relative;
   width: ${ this.getAttribute('width') }px;
   height: ${ this.getAttribute('height') }px;
   margin: 8px 0;
 }
 
-.${ this.divId }.root.clicked {
+.root.clicked {
   background: #000;
 }
 
-.${ this.divId } .preview {
+.preview {
   position: absolute;
   width: 100%;
   height: 100%;
@@ -162,11 +81,7 @@ class VideoPreview extends HTMLElement {
   left: 0;
 }
 
-.${ this.divId } .preview.loaded {
-  cursor: pointer;
-}
-
-.${ this.divId } .preview video {
+.preview video {
   width: 100%;
   height: 100%;
   object-fit: cover;
@@ -174,38 +89,8 @@ class VideoPreview extends HTMLElement {
   transition: opacity .5s ease-out;
 }
 
-.${ this.divId } .preview video.loaded {
+.preview video.loaded {
   opacity: 1;
-}
-
-.${ this.divId } .preview.hidden {
-  display: none;
-}
-
-.${ this.divId } .preview svg {
-  position: absolute;
-  width: 68px;
-  height: 48px;
-  left: 50%;
-  top: 50%;
-  opacity: 0;
-  transform: translate(-50%, -50%);
-  transition: opacity .75s ease-in-out;
-}
-
-.${ this.divId } .preview svg.loaded {
-  opacity: 1;
-}
-
-.${ this.divId } .ytp-large-play-button-bg {
-  transition: fill .1s cubic-bezier(0.4, 0.0, 1, 1), fill-opacity .1s cubic-bezier(0.4, 0.0, 1, 1);
-  fill: #fff;
-  fill-opacity: 1;
-}
-
-.${ this.divId }.root:hover .ytp-large-play-button-bg {
-  transition: fill .1s cubic-bezier(0.0,0.0,0.2,1), fill-opacity .1s cubic-bezier(0.0,0.0,0.2,1);
-  fill: #f00;
 }
     `;
 
